73c9117160cb3dc6996612dff6979ffdf43e4c30
Stefan Alfbo <stefan@alfbo.se>
2025-03-01 22:56:22 +0100

    Add comments to the basic example

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -10,19 +10,23 @@ import (
 )
 
 func main() {
+	// Load environment variables from .env file
 	err := godotenv.Load()
-
 	if err != nil {
 		log.Fatalf("error loading .env file: %v", err)
 	}
 
+	// Create a new langchain model
 	llm, err := openai.New()
 	if err != nil {
 		log.Fatalf("error creating langchain model: %v", err)
 	}
 
+	// Creates a new background context, which is used for managing timeouts
+	// and cancellations in API requests..
 	ctx := context.Background()
 
+	// Requests a completion for the given prompt.
 	completion, err := llm.Call(ctx, "Write a 'Hello, World!' program in Go.")
 	if err != nil {
 		log.Fatalf("error calling langchain model: %v", err)
